053132024e17a82ee062510d0f6c7b719141341e
Ross A. Baker <ross@rossabaker.com>
2016-09-15 10:58:40 -0400

    WIP: refactored Basic auth

--- server/src/main/scala/org/http4s/server/middleware/authentication/package.scala
+++ server/src/main/scala/org/http4s/server/middleware/authentication/package.scala
@@ -2,17 +2,13 @@ package org.http4s
 package server
 package middleware
 
-import scalaz._
-import scalaz.concurrent.Task
 import org.http4s.headers._
+import scalaz._, Scalaz._
+import scalaz.concurrent.Task
 
 package object authentication {
-  // A function mapping (realm, username) to password, None if no password
-  // exists for that (realm, username) pair.
-  type AuthenticationStore = (String, String) => Task[Option[String]]
-
-  def challenged[A](challenge: Service[Request, Challenge \/ AuthedRequest[A]])
-                   (service: AuthedService[A]): HttpService =
+  def challenge[A](challenge: Service[Request, Challenge \/ AuthedRequest[A]])
+                  (service: AuthedService[A]): HttpService =
     Service.lift { req =>
       challenge(req) flatMap {
         case \/-(authedRequest) =>
@@ -21,4 +17,33 @@ package object authentication {
           Task.now(Response(Status.Unauthorized).putHeaders(`WWW-Authenticate`(challenge)))
       }
     }
+
+  type BasicAuthStore[A] = Service[BasicCredentials, AuthReply[A]]
+
+  /**
+    * Provides Basic Authentication from RFC 2617.
+    * @param realm The realm used for authentication purposes.
+    * @param store Maps basic credentials to an auth reply
+    */
+  def basicAuth[A](realm: String, store: BasicAuthStore[A]): AuthMiddleware[A] = {
+    val checkAuth: Service[Request, (Request, AuthReply[A])] =
+      Service.lift { req: Request =>
+        (req.headers.get(Authorization) match {
+          case Some(Authorization(creds: BasicCredentials)) =>
+            store(creds)
+          case Some(Authorization(_)) =>
+            Task.now(AuthFailed)
+          case None =>
+            Task.now(AuthFailed)
+        }).map((req, _))
+      }
+
+    challenge(checkAuth map {
+      case (req, AuthOk(user)) =>
+        AuthedRequest(user, req).right
+      case (_, AuthFailed) =>
+        Challenge("Basic", realm, Map.empty).left
+    })
+  }
 }
+ 
